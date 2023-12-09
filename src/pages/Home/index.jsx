c3d9c52981b36bc96ccf6536e5ebf214bc135933
Nimish Kumar <nimishk642@gmail.com>
2023-12-09 19:53:18 +0530

    Added tags

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,25 @@
 import { Card } from "@components";
+import { FaHeart } from "react-icons/fa";
+import { MdOutlineWork } from "react-icons/md";
 import styles from "./home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.home}>
       <div className={styles.about}>
-        <span>I am fullstack developer.</span>
+        <span style={{ marginBottom: "4rem" }}>
+          Hi, I am fullstack developer.
+        </span>
+        <span className={styles.tag}>
+          <span>#&nbsp;</span>
+          <FaHeart color="red" size="2rem" className={styles.heart} />
+          <span>&nbsp;to build</span>
+        </span>
+        <span className={styles.tag}>
+          <span>#&nbsp;Open</span>
+          <span>&nbsp;to work</span>
+          <MdOutlineWork color="white" size="2rem" />
+        </span>
       </div>
       <div className={styles.card}>
         <Card />
